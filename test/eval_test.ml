4b644982f3f9371b92002a03402009b3d6c920b4
Tymoteusz Wołodźko <twolodzko+git@gmail.com>
2023-03-07 23:31:48 +0100

    Fix tests

--- test/eval_test.ml
+++ test/eval_test.ml
@@ -1,10 +1,11 @@
 open Eval
 open Term
-open Parser
 open Reader
+open Reduce
+open Lexing
 
 let parse_eval s =
-    eval (read (of_string s))
+    eval (read (from_string s))
 
 let%test _ =
   parse_eval "x"
@@ -33,40 +34,57 @@ let%test _ =
 let%test _ =
   parse_eval "(\\ b x y . b x y) (\\ x y . x) t u"
   = Var "t"
+let%test _ =
+  let _ = parse_eval "a = (\\x.x) ok" in
+  (Env.find "a" !env) = Var "ok"
+let%test _ =
+  let _ = parse_eval "a b = a b" in true
+let%test _ =
+  let _ = parse_eval "\\x.x = \\y.y" in true
+let%test _ =
+  let _ = parse_eval "\\x.x = (\\y.y) (\\z.z)" in true
+let%test _ =
+  let _ = parse_eval "(\\y.y) (\\z.z) = \\x.x" in true
+let%test _ =
+  try
+    let _ = parse_eval "x = 1" in
+    let _ = parse_eval "x = 1" in
+    let _ = parse_eval "x = 2" in false
+  with Not_equal _ -> true
 
 let%test _ =
   equal
-    (read (of_string "x"))
-    (read (of_string "x"))
+    (read (from_string "x"))
+    (read (from_string "x"))
 let%test _ =
   not (equal
-    (read (of_string "x"))
-    (read (of_string "y")))
+    (read (from_string "x"))
+    (read (from_string "y")))
 let%test _ =
   equal
-    (read (of_string "t u"))
-    (read (of_string "t u"))
+    (read (from_string "t u"))
+    (read (from_string "t u"))
 let%test _ =
   not (equal
-    (read (of_string "u t"))
-    (read (of_string "t u")))
+    (read (from_string "u t"))
+    (read (from_string "t u")))
 let%test _ =
   equal
-    (read (of_string "\\ x . x"))
-    (read (of_string "\\ x . x"))
+    (read (from_string "\\ x . x"))
+    (read (from_string "\\ x . x"))
 let%test _ =
   equal
-    (read (of_string "\\ x . x"))
-    (read (of_string "\\ y . y"))
+    (read (from_string "\\ x . x"))
+    (read (from_string "\\ y . y"))
 let%test _ =
   equal
-    (read (of_string "\\ x y . x y"))
-    (read (of_string "\\ y x . y x"))
+    (read (from_string "\\ x y . x y"))
+    (read (from_string "\\ y x . y x"))
 let%test _ =
   equal
-    (read (of_string "\\ x y . x (y z)"))
-    (read (of_string "\\ y x . y (x z)"))
+    (read (from_string "\\ x y . x (y z)"))
+    (read (from_string "\\ y x . y (x z)"))
 let%test _ =
   equal
-    (read (of_string "(\\ x . x) z"))
-    (read (of_string "(\\ y . y) z"))
+    (read (from_string "(\\ x . x) z"))
+    (read (from_string "(\\ y . y) z"))
